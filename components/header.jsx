b597d314f55cda94db61fbfb20a50cc74b81e669
Thomas <thomashaudiquer@gmail.com>
2023-04-24 11:47:04 +0100

    commit

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="bg-red-500 p-5">
+      <h1 className=''>Header</h1>
+      <p className='font-bold'>test</p>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from '../../components/Header.jsx';
 import CarrouselMain from '../../components/CarrouselMain.jsx';
 import AboutUs from '../../components/aboutUs.jsx';

c00f8eba36c4d6793469174829f4b4e0b8cb4732
Thomas <thomashaudiquer@gmail.com>
2023-04-25 12:28:30 +0100

    marquee done

--- components/Marquee.jsx
+++ components/Marquee.jsx
@@ -5,12 +5,16 @@ function Marquee() {
       <div class="relative flex overflow-x-hidden uppercase text-6xl text-main">
         <div class="py-12 animate-marquee whitespace-nowrap">
           <span class="mx-4">Free returns & exchanges</span>
+          <span class="mx-4">💍</span>
           <span class="mx-4">Complimentary resizing for life</span>
+          <span class="mx-4">💍</span>
         </div>
 
         <div class="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
           <span class="mx-4">Free returns & exchanges</span>
+          <span class="mx-4">💍</span>
           <span class="mx-4">Complimentary resizing for life</span>
+          <span class="mx-4">💍</span>
         </div>
       </div>
   )

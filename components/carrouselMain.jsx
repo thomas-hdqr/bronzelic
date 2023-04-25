2174b82be8d3e42731f32ba11a1987866f3af7ce
Thomas <thomashaudiquer@gmail.com>
2023-04-25 10:43:42 +0100

    1st element

--- components/carrouselMain.jsx
+++ components/carrouselMain.jsx
@@ -7,18 +7,35 @@ function CarrouselMain() {
   
   return (
 
+    <div className='p-10'>
 
-    <div className="w-full grid-cols-3 gap-x-2.5">
+    <div className="relative w-full uppercase text-main text-xs">
+      <div className="absolute left-0">
+        <p className="inline-block">custom made just for you</p>
+      </div>
+      <div className="w-full flex justify-center">
+        <p className="inline-block">select a ring style to begin customizing</p>
+      </div>
+    </div>
 
 
-      <div className="w-full relative ">
-        <div className='w-full flex flex-col items-center justify-center gap-y-2.5 px-5'>
-          <p className="uppercase text-xs">engagement rings</p>
+
+
+
+      <div className="w-full grid-cols-3 gap-x-2.5">
+
+
+        <div className="w-full relative ">
+          <div className='w-full flex flex-col items-center justify-center gap-y-2.5 px-5'>
+            <p className="uppercase text-xs">engagement rings</p>
+          </div>
         </div>
+
+
       </div>
 
-
     </div>
+
   )
 }
 

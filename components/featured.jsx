1de22d434c8503e754a53d6509c52eda5832a007
Thomas <thomashaudiquer@gmail.com>
2023-04-25 15:10:46 +0100

    commit

--- components/featured.jsx
+++ components/featured.jsx
@@ -1,8 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Featured() {
   return (
-    <div>Featured</div>
+    <div className="bg-[#b0a38b] py-10">
+      <div>
+        <p className="text-[#59552D] uppercase text-center text-xs">as featured in</p>
+      </div>
+      <div className='flex px-40 py-10 gap-x-24'>
+        <div className='width-[110px] mx-auto'>
+          <Image src="/vogue.webp" alt="" width={110} height={200} />
+        </div>
+        <div className='width-[110px] mx-auto'>
+          <Image src="/forbes.webp" alt="" width={110} height={200} />
+        </div>
+        <div className='width-[110px] mx-auto'>
+          <Image src="/brides.webp" alt="" width={110} height={200} />
+        </div>
+        <div className='width-[110px] mx-auto'>
+          <Image src="/the_knot.webp" alt="" width={110} height={200} />
+        </div>
+        <div className='width-[110px] mx-auto'>
+          <Image src="/refinery29.webp" alt="" width={110} height={200} />
+        </div>
+      </div>
+    </div>
   )
 }
 

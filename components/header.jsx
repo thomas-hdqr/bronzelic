f47c8a8a318e0931a95001647269f3438356bff6
Thomas <thomashaudiquer@gmail.com>
2023-04-25 09:37:19 +0100

    navbar done

--- components/header.jsx
+++ components/header.jsx
@@ -1,33 +1,58 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+// ./components/Header.js
+import { useState, useEffect } from 'react';
 import { MagnifyingGlassIcon,
-         ShoppingBagIcon
- } from '@heroicons/react/24/outline'
+  ShoppingBagIcon
+} from '@heroicons/react/24/outline'
 
-function Header() {
+const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
 
-  
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 150) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className="uppercase top-0 sticky w-full justify-between items-center flex z-50 px-5 h-14 text-xs">
-      <div className="flex items-center justify-between gap-x-5 cursor-pointer">
-        <p>shop</p>
-        <p>about us</p>
-        <p>journal</p>
-        <p>free size kit</p>
+    <header
+      className={`fixed w-full z-50 py-4 transition-all duration-300 ease-in-out items-center flex h-14 ${
+        scrolled ? 'bg-white' : 'bg-transparent'
+      }`}
+    >
+      {/* Header content here */}
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center uppercase">
+          <div className={`flex items-center justify-between gap-x-5 cursor-pointer text-xs ${scrolled ? 'text-main' : 'text-white'}`}>
+            <p>shop</p>
+            <p>about us</p>
+            <p>journal</p>
+            <p>free size kit</p>
+          </div>
+          <div className={`text-2xl font-bold cursor-pointer ${scrolled ? 'text-main' : 'text-white'}`}>
+            {/* Your logo or brand name */}
+            <h1>bronzelic</h1>
+          </div>
+          {/* Navigation items */}
+          <div className={`flex items-center justify-between gap-x-5 cursor-pointer text-xs ${scrolled ? 'text-main' : 'text-white'}`}>
+            <p>reviews</p>
+            <p>appointments</p>
+            <MagnifyingGlassIcon className='h-5'/>
+            <ShoppingBagIcon className='h-5'/>
+          </div>
+        </div>
       </div>
-      <div className='cursor-pointer'>
-        <h1>bronzelic</h1>
-      </div>
-      <div className="flex items-center justify-between gap-x-5 cursor-pointer">
-        <p>reviews</p>
-        <p>appointments</p>
-        <MagnifyingGlassIcon className='h-5'/>
-        <ShoppingBagIcon className='h-5'/>
-      </div>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
-
+export default Header;

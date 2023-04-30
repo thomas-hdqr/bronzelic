1b89567280a2374f126ba830d6a27f06232e3ee8
Thomas <thomashaudiquer@gmail.com>
2023-04-30 09:19:01 +0100

    commit

--- components/subscribe.jsx
+++ components/subscribe.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Subscribe() {
   return (
-    <div>subscribe</div>
+    <div>
+      <div>
+        <p>Stay in Touch</p>
+      </div>
+      <div>
+        <form>
+          <input type="text" placeholder='Your Email Adress'/>
+        </form>
+      </div>
+      <div className="bg-[#59552D] uppercase text-xs text-white py-5 px-10 transition duration-200 ease-in cursor-pointer">
+          enter
+      </div>
+    </div>
   )
 }
 

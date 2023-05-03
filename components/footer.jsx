4342eb574eb00dd8e3f997189d995271d4358f62
Thomas <thomashaudiquer@gmail.com>
2023-05-03 11:37:38 +0100

    commit

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,7 +2,45 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="grid grid-cols-5 uppercase text-xs text-main p-10">
+
+      <div>
+        <p className="text-[#59552e9c] pb-4">give us a ring</p>
+        <p className="cursor-pointer">help@bronzelic</p>
+        <p className="cursor-pointer">912.273.2632</p>
+        <p className="cursor-pointer">23 5 ave New york</p>
+      </div>
+      <div>
+        <p className="text-[#59552e9c] pb-4">about</p>
+        <p className="cursor-pointer">about us</p>
+        <p className="cursor-pointer">custom & bespoke</p>
+        <p className="cursor-pointer">reviews</p>
+        <p className="cursor-pointer">sustainability</p>
+        <p className="cursor-pointer">journal</p>
+      </div>
+      <div>
+        <p className="text-[#59552e9c] pb-4">help</p>
+        <p className="cursor-pointer">faq</p>
+        <p className="cursor-pointer">lifetime guarantee</p>
+        <p className="cursor-pointer">shipping & returns</p>
+        <p className="cursor-pointer">contact</p>
+        <p className="cursor-pointer">my account</p>
+        <p className="cursor-pointer">free size kit</p>
+      </div>
+      <div>
+        <p className="text-[#59552e9c] pb-4">social</p>
+        <p className="cursor-pointer">instagram</p>
+        <p className="cursor-pointer">facebook</p>
+        <p className="cursor-pointer">pinterest</p>
+        <p className="cursor-pointer">tik tok</p>
+      </div>
+      <div>
+        <p className="text-[#59552e9c] pb-4">privacy</p>
+        <p className="cursor-pointer">terms and privacy</p>
+        <p className="cursor-pointer">do not sell my info</p>
+      </div>
+
+    </div>
   )
 }
 

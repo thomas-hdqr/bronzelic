95db0bc5063c7ddbd4e52c0d22f2b7bb879ba56b
Thomas <thomashaudiquer@gmail.com>
2023-05-03 11:17:34 +0100

    subscribe button done

--- components/subscribe.jsx
+++ components/subscribe.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Subscribe() {
   return (
-    <div className='flex flex-row gap-x-10 m-10 p-4 border-y border-main'>
+    <div className='flex flex-row items-center gap-x-10 m-10 p-4 border-y border-main text-xl'>
       <div className=''>
-        <p className='text-main text-2xl font-noto italic'>Stay in Touch</p>
+        <p className='text-main font-noto italic'>Stay in Touch</p>
       </div>
       <div className='flex-1'>
         <form>
-          <input className='text-main text-xl' type="text" placeholder='Your Email Adress'/>
+          <input className='text-main focus:border-none focus:outline-none placeholder-[#59552e9c]' type="text" placeholder='Your Email Adress'/>
         </form>
       </div>
-      <div className="bg-[#59552D] uppercase text-xs text-white py-6 px-16 transition duration-200 ease-in cursor-pointer">
+      <div className="bg-[#59552D] uppercase text-white py-4 px-16 transition duration-200 ease-in cursor-pointer">
           enter
       </div>
     </div>

dca7b5aa7f4b4d511f2c1a5f317a249fab899461
Thomas <thomashaudiquer@gmail.com>
2023-05-05 10:17:54 +0100

    commit footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,23 +5,23 @@ function Footer() {
 
     <div className="px-10">
 
-      <div className="grid grid-cols-5 uppercase text-xs text-main pb-16">
-        <div>
-          <p className="text-[#59552e9c] pb-4">give us a ring</p>
+      <div className="grid sm:grid-cols-5 grid-cols-2 uppercase text-xs text-main pb-16">
+        <div className="">
+          <p className="text-[#59552e9c] py-4">give us a ring</p>
           <p className="cursor-pointer hover:underline">help@bronzelic</p>
           <p className="">912.273.2632</p>
           <p className="">23 5 ave New york</p>
         </div>
-        <div>
-          <p className="text-[#59552e9c] pb-4">about</p>
+        <div className="">
+          <p className="text-[#59552e9c] py-4">about</p>
           <p className="cursor-pointer hover:underline">about us</p>
           <p className="cursor-pointer hover:underline">custom & bespoke</p>
           <p className="cursor-pointer hover:underline">reviews</p>
           <p className="cursor-pointer hover:underline">sustainability</p>
           <p className="cursor-pointer hover:underline">journal</p>
         </div>
-        <div>
-          <p className="text-[#59552e9c] pb-4">help</p>
+        <div className="">
+          <p className="text-[#59552e9c] py-4">help</p>
           <p className="cursor-pointer hover:underline">faq</p>
           <p className="cursor-pointer hover:underline">lifetime guarantee</p>
           <p className="cursor-pointer hover:underline">shipping & returns</p>
@@ -29,15 +29,15 @@ function Footer() {
           <p className="cursor-pointer hover:underline">my account</p>
           <p className="cursor-pointer hover:underline">free size kit</p>
         </div>
-        <div>
-          <p className="text-[#59552e9c] pb-4">social</p>
+        <div className="">
+          <p className="text-[#59552e9c] py-4">social</p>
           <p className="cursor-pointer hover:underline">instagram</p>
           <p className="cursor-pointer hover:underline">facebook</p>
           <p className="cursor-pointer hover:underline">pinterest</p>
           <p className="cursor-pointer hover:underline">tik tok</p>
         </div>
-        <div>
-          <p className="text-[#59552e9c] pb-4">privacy</p>
+        <div className="">
+          <p className="text-[#59552e9c] py-4">privacy</p>
           <p className="cursor-pointer hover:underline">terms and privacy</p>
           <p className="cursor-pointer hover:underline">do not sell my info</p>
         </div>

b666648e38fe9256798e6e81ab6e4d51db930733
Thomas <thomashaudiquer@gmail.com>
2023-11-14 11:40:51 +0000

    edit

--- components/CollectionItem.jsx
+++ components/CollectionItem.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function CollectionItem({ imageUrl, title, price }) {
   return (
-    <div className="sm:w-full grid items-center justify-center gap-y-3.5 py-14 px-5">
+    <div className="sm:hover:border hover:border-[#59552D] transition duration-400 ease-in sm:w-full   grid items-center justify-center gap-y-3.5 py-14 px-5">
       <div className="border-b border-[#59552D] py-4 w-96 h-96 relative">
         <Image src={imageUrl} alt="title" fill className="object-cover py-4" />
       </div>

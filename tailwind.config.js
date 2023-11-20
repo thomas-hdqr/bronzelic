8076011cd4ff14e2867a71f3157186a724052ccd
Thomas <thomashaudiquer@gmail.com>
2023-11-20 15:03:51 +0000

    commitssss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
     "./src/**/*.{html,js}"
   ],
   theme: {

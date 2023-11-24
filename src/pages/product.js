5ea404b4c4f8b5f5b6e684be84d70033ae21614e
Thomas <thomashaudiquer@gmail.com>
2023-11-24 15:24:36 +0000

    commit

--- src/pages/product.js
+++ src/pages/product.js
@@ -9,16 +9,34 @@ export default function Product() {
     <div>
       <Header isSpecialPage={true}/>
 
-      <div className="h-screen grid sm:grid-cols-3">
-        <div className="bg-blue-100 border border-[#59552D]">
-          <div className="rounded-full ">
-            <p>1</p>
-          </div>
-            <p>Customize yours</p>
-        </div>
-        <div className="bg-yellow-100"></div>
-        <div className="bg-green-100"></div>
-      </div>
+      <div className="grid grid-cols-3 divide-x divide-[#59552D]">
+  {/* Step 1 */}
+  <div className="flex flex-col items-center justify-center py-4">
+    <div className="w-8 h-8 rounded-full bg-[#FFE6AA] flex items-center justify-center">
+      <span className="font-bold">1</span>
+    </div>
+    <p className="text-xs mt-2">Customize Yours</p>
+  </div>
+
+  {/* Step 2 */}
+  <div className="flex flex-col items-center justify-center py-4">
+    <div className="w-8 h-8 rounded-full bg-[#EFEFEF] flex items-center justify-center">
+      <span className="font-bold">2</span>
+    </div>
+    <p className="text-xs mt-2">Choose Your Diamond</p>
+  </div>
+
+  {/* Step 3 */}
+  <div className="flex flex-col items-center justify-center py-4">
+    <div className="w-8 h-8 rounded-full bg-[#D2D2D2] flex items-center justify-center">
+      <span className="font-bold">3</span>
+    </div>
+    <p className="text-xs mt-2">Confirm</p>
+  </div>
+</div>
+
+
+
       <div className="w-40 h-40 rounded-full  
                 inline-flex items-center justify-center  
                 bg-white text-gray-700 text-xl font-bold">test</div>

6588de071eda5999e4986f374fbcb490da004d22
Thomas <thomashaudiquer@gmail.com>
2023-11-24 15:45:54 +0000

    header

--- components/header.jsx
+++ components/header.jsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from 'react';
-import { 
+import { useState, useEffect } from "react";
+import {
   MagnifyingGlassIcon,
   ShoppingBagIcon,
   ChevronRightIcon,
-} from '@heroicons/react/24/outline'
-import { Cross as Hamburger } from 'hamburger-react'
-import Link from 'next/link';
+} from "@heroicons/react/24/outline";
+import { Cross as Hamburger } from "hamburger-react";
+import Link from "next/link";
 
-const Header = ( {isSpecialPage} ) => {
-
+const Header = ({ isSpecialPage }) => {
   const [scrolled, setScrolled] = useState(false);
   const [toggle, setToggle] = useState(false);
 
   const headerClass = `hidden sm:flex fixed w-full z-50 py-4 transition-all duration-300 ease-in-out items-center h-14 ${
-    isSpecialPage ? 'bg-red-400 text-main border-1 border-main' : (scrolled ? 'bg-white' : 'bg-transparent')
+    isSpecialPage || scrolled ? "bg-white" : "bg-transparent"
   }`;
 
-  const textClass = isSpecialPage ? 'text-main' : (scrolled ? 'text-main' : 'text-white');
+  const textClass = `text-xs ${
+    isSpecialPage || scrolled ? "text-black" : "text-white"
+  }`;
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -28,88 +29,123 @@ const Header = ( {isSpecialPage} ) => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-
     <div>
-
-
       {/* Desktop version */}
       <header className={headerClass}>
         {/* Header content here */}
         <div className="container mx-auto px-4">
           <div className="flex justify-between items-center uppercase">
-            <div className={`flex items-center justify-between gap-x-5 cursor-pointer text-xs ${scrolled ? 'text-main' : 'text-white'}`}>
+            <div
+              className={`flex items-center justify-between gap-x-5 cursor-pointer ${textClass}`}
+            >
               <p>shop</p>
               <p>about us</p>
               <p>journal</p>
               <p>free size kit</p>
             </div>
             <Link href="/">
-              <div className={`text-2xl font-bold cursor-pointer ${scrolled ? 'text-main' : 'text-white'}`}>
+              <div className={` font-bold cursor-pointer ${textClass}`}>
                 <h1>bronzelic</h1>
               </div>
             </Link>
             {/* Navigation items */}
-            <div className={`flex items-center justify-between gap-x-5 cursor-pointer text-xs ${scrolled ? 'text-main' : 'text-white'}`}>
+            <div
+              className={`flex items-center justify-between gap-x-5 cursor-pointer ${textClass}`}
+            >
               <p>reviews</p>
               <p>appointments</p>
-              <MagnifyingGlassIcon className='h-5'/>
-              <ShoppingBagIcon className='h-5'/>
+              <MagnifyingGlassIcon className="h-5" />
+              <ShoppingBagIcon className="h-5" />
             </div>
           </div>
         </div>
       </header>
 
-
       {/* Responsive version */}
       <div
-  className={`sm:hidden flex fixed w-full z-50 p-4 transition-all duration-300 ease-in-out items-center h-14 ${
-    scrolled ? 'bg-white' : 'bg-transparent'
-  }`}
->
-  <div className="flex flex-grow justify-start items-center">
-    <div className="w-5 cursor-pointer">
-      <Hamburger color="black" toggled={toggle} toggle={setToggle} />
-    </div>
-  </div>
-  <div className="flex-grow flex justify-center items-center">
-    <h1 className="text-2xl font-bold uppercase cursor-pointer">bronzelic</h1>
-  </div>
-  <div className="flex flex-grow justify-end items-center space-x-4">
-    <MagnifyingGlassIcon className="h-5 cursor-pointer" />
-    <ShoppingBagIcon className="h-5 cursor-pointer" />
-  </div>
-</div>
+        className={`sm:hidden flex fixed w-full z-50 p-4 transition-all duration-300 ease-in-out items-center h-14 ${
+          scrolled ? "bg-white" : "bg-transparent"
+        }`}
+      >
+        <div className="flex flex-grow justify-start items-center">
+          <div className="w-5 cursor-pointer">
+            <Hamburger color="black" toggled={toggle} toggle={setToggle} />
+          </div>
+        </div>
+        <div className="flex-grow flex justify-center items-center">
+          <h1 className="text-2xl font-bold uppercase cursor-pointer">
+            bronzelic
+          </h1>
+        </div>
+        <div className="flex flex-grow justify-end items-center space-x-4">
+          <MagnifyingGlassIcon className="h-5 cursor-pointer" />
+          <ShoppingBagIcon className="h-5 cursor-pointer" />
+        </div>
+      </div>
 
       {toggle && (
         <div className="fixed top-0 left-0 w-full h-full z-40 bg-[#868159]">
           {/* Add your menu items here */}
           <ul className="px-8 pt-20 text-white text-2xl space-y-2 uppercase">
-            <li onClick={() => setToggle(false)} className="cursor-pointer hover:underline flex flex-row gap-x-4">shop <ChevronRightIcon className='h-8'/></li>
-            <li onClick={() => setToggle(false)} className="cursor-pointer hover:underline flex flex-row gap-x-4">about us <ChevronRightIcon className='h-8'/></li>
-            <li onClick={() => setToggle(false)} className="cursor-pointer hover:underline flex flex-row gap-x-4">journal <ChevronRightIcon className='h-8'/></li>
-            <li onClick={() => setToggle(false)} className="cursor-pointer hover:underline">free size kit</li>
-            <li onClick={() => setToggle(false)} className='pt-8 cursor-pointer hover:underline'>reviews</li>
-            <li onClick={() => setToggle(false)} className="cursor-pointer hover:underline">appointments</li>
-            <li onClick={() => setToggle(false)} className='flex flex-row gap-x-4 cursor-pointer hover:underline'>Search <MagnifyingGlassIcon className='h-8'/></li>
-            <li onClick={() => setToggle(false)} className='flex flex-row gap-x-4 cursor-pointer hover:underline'>Bag <ShoppingBagIcon className='h-8'/></li>
+            <li
+              onClick={() => setToggle(false)}
+              className="cursor-pointer hover:underline flex flex-row gap-x-4"
+            >
+              shop <ChevronRightIcon className="h-8" />
+            </li>
+            <li
+              onClick={() => setToggle(false)}
+              className="cursor-pointer hover:underline flex flex-row gap-x-4"
+            >
+              about us <ChevronRightIcon className="h-8" />
+            </li>
+            <li
+              onClick={() => setToggle(false)}
+              className="cursor-pointer hover:underline flex flex-row gap-x-4"
+            >
+              journal <ChevronRightIcon className="h-8" />
+            </li>
+            <li
+              onClick={() => setToggle(false)}
+              className="cursor-pointer hover:underline"
+            >
+              free size kit
+            </li>
+            <li
+              onClick={() => setToggle(false)}
+              className="pt-8 cursor-pointer hover:underline"
+            >
+              reviews
+            </li>
+            <li
+              onClick={() => setToggle(false)}
+              className="cursor-pointer hover:underline"
+            >
+              appointments
+            </li>
+            <li
+              onClick={() => setToggle(false)}
+              className="flex flex-row gap-x-4 cursor-pointer hover:underline"
+            >
+              Search <MagnifyingGlassIcon className="h-8" />
+            </li>
+            <li
+              onClick={() => setToggle(false)}
+              className="flex flex-row gap-x-4 cursor-pointer hover:underline"
+            >
+              Bag <ShoppingBagIcon className="h-8" />
+            </li>
           </ul>
         </div>
       )}
-    
-
-
-
-
-
     </div>
-
   );
 };
 

e26a794946cf6cf804512c3d16c4eb35dcb64bbb
Thomas <thomashaudiquer@gmail.com>
2024-02-26 15:20:26 +0000

    Update Image import to use next/legacy/image

--- components/instagram.jsx
+++ components/instagram.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useRef, useEffect } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
